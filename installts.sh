09b3e12d231ee3cf35b961a9e0cb07868c83868b
Mohammad Shaygan <mpgbieghesh468@gmail.com>
2025-12-31 13:08:31 +0330

    Add files via upload

---
+++ installts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd
+cd Downloads
+
+chmod +x TeamSpeak3-Client-linux_amd64-3.6.2.run
+./TeamSpeak3-Client-linux_amd64-3.6.2.run
+
+mv TeamSpeak3-Client-linux_amd64 /home/$USER/opt
+
+
---
+++ ts3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#!/bin/bash
+clear && cd
+echo -e "\e[32mRuning TeamSpeak3 client...\e[0m"
+sleep 2
+
+for load in {1..5}; do
+    clear
+    echo  "Loading."
+    sleep 0.3
+    clear
+    echo "Loading.."
+    sleep 0.3
+    clear
+    echo  "Loading..."
+    sleep 0.3
+    clear
+done
+
+cd opt
+bash ts3client_runscript.sh
+
